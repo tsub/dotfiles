2c670bb07c0503de40b88b31a42320b4839f39c3
tsub <tsubasatakayama511@gmail.com>
2017-05-02 18:05:25 +0900

    google-cloud-sdk install with homebrew cask

--- cookbooks/zsh/files/.zsh/settings/tools.zsh
+++ cookbooks/zsh/files/.zsh/settings/tools.zsh
@@ -42,11 +42,11 @@ fi
 ## gcloud
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f ${HOME}/.google-cloud-sdk/path.zsh.inc ]; then
-  source "$HOME/.google-cloud-sdk/path.zsh.inc"
+if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc ]; then
+  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
 fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f ${HOME}/.google-cloud-sdk/completion.zsh.inc ]; then
-  source "$HOME/.google-cloud-sdk/completion.zsh.inc"
+if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc ]; then
+  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 fi
