52fd7d5b8aab2cbb72b69d8389c68f75eb8d926d
Tsubasa Takayama <t-takayama@feedforce.jp>
2017-05-10 10:58:19 +0900

    Remove peco cookbook

--- cookbooks/peco/default.rb
+++
@@ -1,5 +0,0 @@
-package 'peco'
-
-link File.expand_path('~/.peco') do
-  to File.expand_path('../files/.peco', __FILE__)
-end
--- roles/develop.rb
+++ roles/develop.rb
@@ -7,7 +7,6 @@ node.reverse_merge!({
 include_recipe '../cookbooks/anyenv/default.rb'
 include_recipe '../cookbooks/git/default.rb'
 include_recipe '../cookbooks/neovim/default.rb'
-include_recipe '../cookbooks/peco/default.rb'
 include_recipe '../cookbooks/the_silver_searcher/default.rb'
 include_recipe '../cookbooks/tmux/default.rb'
 include_recipe '../cookbooks/zsh/default.rb'
