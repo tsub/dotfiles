b2a39acd86dd92eaa9fbabd598711079a38bf104
Tsubasa Takayama <t-takayama@feedforce.jp>
2017-06-20 11:19:10 +0900

    Fix files path

--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -14,13 +14,13 @@ end
 rbenv '2.4.1'
 
 link File.expand_path('~/.gemrc') do
-  to File.expand_path('../../files/.gemrc', __FILE__)
+  to File.expand_path('../files/.gemrc', __FILE__)
 end
 
 link File.expand_path('~/.bundle') do
-  to File.expand_path('../../files/.bundle', __FILE__)
+  to File.expand_path('../files/.bundle', __FILE__)
 end
 
 link File.expand_path('~/.rbenv/default-gems') do
-  to File.expand_path('../../files/.rbenv/default-gems', __FILE__)
+  to File.expand_path('../files/.rbenv/default-gems', __FILE__)
 end
