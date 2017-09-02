2cfca98b9a4492b1771a5bbb77b80d2a3ccfd6bf
tsub <tsubasatakayama511@gmail.com>
2017-09-02 23:23:34 +0900

    Fix tpm install path
    And add how to setup tpm to README

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 $ git clone git@github.com:tsub/dotfiles.git && cd dotfiles
 $ bin/setup
 $ bin/mitamae local roles/develop.rb
+$ ~/.tmux/plugins/tpm/bin/install_plugins
 ```
--- cookbooks/tmux/default.rb
+++ cookbooks/tmux/default.rb
@@ -6,8 +6,8 @@ link File.expand_path('~/.tmux.conf') do
   to File.expand_path('../files/.tmux.conf', __FILE__)
 end
 
-directory File.expand_path('~/.tpm/plugins')
+directory File.expand_path('~/.tmux/plugins')
 
-git File.expand_path('~/.tpm/plugins/tpm') do
+git File.expand_path('~/.tmux/plugins/tpm') do
   repository 'https://github.com/tmux-plugins/tpm.git'
 end
