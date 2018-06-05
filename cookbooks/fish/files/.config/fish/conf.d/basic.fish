fd131ca67fab8608af69a652b06989c6dbfbd10d
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-06-05 14:26:01 +0900

    Add /usr/local/sbin in $PATH

--- cookbooks/fish/files/.config/fish/conf.d/basic.fish
+++ cookbooks/fish/files/.config/fish/conf.d/basic.fish
@@ -3,6 +3,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export PAGER="less"
 export LESS="-g -i -M -R -S -W -z-4 -x4"
 export PATH="$HOME/.bin:$PATH"
+export PATH="$PATH:/usr/local/sbin"
 
 ## Set Locale
 export LC_ALL="en_US.UTF-8"
