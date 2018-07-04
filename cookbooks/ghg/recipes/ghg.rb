6dc01e6712f81756fdefc89378edd76d78b4cf10
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-07-04 14:04:08 +0900

    Workaround to specify cli_name

--- cookbooks/ghg/recipes/ghg.rb
+++ cookbooks/ghg/recipes/ghg.rb
@@ -1,6 +1,8 @@
 ghg 'GetStream/vg'
 ghg 'heptio/authenticator'
 ghg 'hyperhq/pi'
-ghg 'turnerlabs/fargate'
+ghg 'turnerlabs/fargate' do
+  cli_name 'ara'
+end
 ghg 'mattn/memo'
 ghg 'wantedly/dockertags'
