44802f138bce74d0a070ce2cd9a77af9edea702a
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-07-07 22:00:45 +0900

    Create CLI alias

--- cookbooks/ghg/default.rb
+++ cookbooks/ghg/default.rb
@@ -13,13 +13,19 @@ execute "mv /tmp/ghg_v#{node[:ghg][:version]}_darwin_amd64/ghg /usr/local/bin" d
   not_if 'test -x /usr/local/bin/ghg'
 end
 
-define :ghg, version: nil, cli_name: nil do
+define :ghg, version: nil, cli_name: nil, alias_name: nil do
   # workaround: Avoid nil to string -> '[]'
   version = params[:version] ? "@v#{params[:version]}" : ''
 
   execute "ghg get #{params[:name]}#{version}" do
     tool_name = params[:cli_name] || params[:name].split('/').last
     not_if "test -x ~/.ghg/bin/#{tool_name}"
+  end
+
+  if params[:alias_name] && params[:cli_name]
+    link File.expand_path("~/.ghg/bin/#{params[:alias_name]}") do
+      to File.expand_path("~/.ghg/bin/#{params[:cli_name]}")
+    end
   end
 end
 
--- cookbooks/ghg/recipes/ghg.rb
+++ cookbooks/ghg/recipes/ghg.rb
@@ -1,6 +1,7 @@
 ghg 'hyperhq/pi'
 ghg 'kubernetes-sigs/aws-iam-authenticator' do
   cli_name 'i-authenticator'
+  alias_name 'heptio-authenticator-aws'
 end
 ghg 'mattn/memo'
 ghg 'wantedly/dockertags'
