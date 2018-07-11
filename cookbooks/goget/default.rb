26e5fbbf08ccce0050b0999bde73d1f7cd2d808d
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-07-11 11:28:35 +0900

    Add goget cookbook

---
+++ cookbooks/goget/default.rb
@@ -0,0 +1,9 @@
+package 'go'
+
+define :goget do
+  execute "go get #{params[:name]}" do
+    not_if "test -d $GOPATH/src/#{params[:name]}"
+  end
+end
+
+include_recipe 'recipes/goget.rb'
---
+++ cookbooks/goget/recipes/goget.rb
@@ -0,0 +1,4 @@
+goget 'github.com/ariarijp/crontoc'
+goget 'github.com/nsf/gocode'
+goget 'golang.org/x/lint/golint'
+goget 'golang.org/x/tools/cmd/goimports'
