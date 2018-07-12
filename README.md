302df1291f9a0c0b66a0514cd03c4cad100aeda0
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-07-12 14:34:11 +0900

    To Automate installing tmux plugins

--- README.md
+++ README.md
@@ -6,5 +6,4 @@
 $ git clone git@github.com:tsub/dotfiles.git && cd dotfiles
 $ bin/setup
 $ bin/mitamae local roles/develop.rb
-$ ~/.tmux/plugins/tpm/bin/install_plugins
 ```
--- cookbooks/tmux/default.rb
+++ cookbooks/tmux/default.rb
@@ -11,3 +11,7 @@ directory File.expand_path('~/.tmux/plugins')
 git File.expand_path('~/.tmux/plugins/tpm') do
   repository 'https://github.com/tmux-plugins/tpm.git'
 end
+
+execute File.expand_path('~/.tmux/plugins/tpm/bin/install_plugins') do
+  not_if 'test -d ~/.tmux/plugins/tmux-prefix-highlight'
+end
