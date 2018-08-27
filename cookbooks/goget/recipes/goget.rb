1613494afc480fc5dfd399fd5f890eea8b69c5cb
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-08-27 22:30:45 +0900

    Replace gocode repository

--- cookbooks/goget/recipes/goget.rb
+++ cookbooks/goget/recipes/goget.rb
@@ -1,5 +1,5 @@
 goget 'github.com/Ladicle/git-prompt'
 goget 'github.com/ariarijp/crontoc'
-goget 'github.com/nsf/gocode'
+goget 'github.com/mdempsky/gocode'
 goget 'golang.org/x/lint/golint'
 goget 'golang.org/x/tools/cmd/goimports'
