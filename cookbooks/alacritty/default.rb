0881773d1814e71b54ab1b0d2cd7640067fe5740
tsub <tsubasatakayama511@gmail.com>
2018-09-17 03:03:17 +0900

    Add alacritty config for Linux

--- cookbooks/alacritty/default.rb
+++ cookbooks/alacritty/default.rb
@@ -2,7 +2,9 @@ directory node[:alacritty][:home] do
   user node[:user]
 end
 
+arch = node[:platform] == 'darwin' ? 'macos' : 'linux'
+
 link "#{node[:alacritty][:home]}/alacritty.yml" do
-  to File.expand_path('../files/alacritty/alacritty.yml', __FILE__)
+  to File.expand_path("../files/alacritty/alacritty.#{arch}.yml", __FILE__)
   user node[:user]
 end
