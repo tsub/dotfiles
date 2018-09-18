4ac281d9d2a1728904999dfc8cd6329b01dcfce9
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-09-18 21:31:31 +0900

    Remove xkeysnail.service

--- cookbooks/service/default.rb
+++ cookbooks/service/default.rb
@@ -1,15 +1,9 @@
-template File.expand_path('/etc/systemd/system/xkeysnail.service') do
-  source File.expand_path('../templates/xkeysnail.service.erb', __FILE__)
-  mode '0644'
-  variables user: node[:user]
-end
-
 remote_file File.expand_path('/etc/systemd/system/powertop.service') do
   source File.expand_path('../files/powertop.service', __FILE__)
   mode '0644'
 end
 
-%w[tlp docker lenovo_fix acpid powertop xkeysnail].each do |s|
+%w[tlp docker lenovo_fix acpid powertop].each do |s|
   service s do
     action [:enable, :start]
   end
