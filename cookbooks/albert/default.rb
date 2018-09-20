7e3449fe51dc3460d582f636aaedc19425504072
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-09-20 21:34:14 +0900

    Add albert cookbook

---
+++ cookbooks/albert/default.rb
@@ -0,0 +1,12 @@
+directory File.expand_path('~/.config') do
+  user node[:user]
+end
+
+directory File.expand_path('~/.config/albert') do
+  user node[:user]
+end
+
+link File.expand_path('~/.config/albert/albert.conf') do
+  to File.expand_path('../files/albert.conf', __FILE__)
+  user node[:user]
+end
