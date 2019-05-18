e75822a0ae5be1add8eb28d1f5a972f5d75887c9
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2019-05-18 11:15:20 +0900

    Specify font style for terminal emulators

--- cookbooks/hyper/files/.hyper.js
+++ cookbooks/hyper/files/.hyper.js
@@ -15,7 +15,7 @@ module.exports = {
     fontFamily: '"FuraCode Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
-    fontWeight: 'normal',
+    fontWeight: 'bold',
 
     // font weight for bold characters: 'normal' or 'bold'
     fontWeightBold: 'bold',
