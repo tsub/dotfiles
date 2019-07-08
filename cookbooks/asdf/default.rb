79c7de0a9a4a9778f7f9d184f2eaab915d3fe5ca
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2019-07-08 11:51:04 +0900

    Fix not run import-release-team-keyring script

--- cookbooks/asdf/default.rb
+++ cookbooks/asdf/default.rb
@@ -2,13 +2,14 @@ define :asdf_plugin do
   execute "asdf plugin-add #{params[:name]}" do
     user node[:user]
     not_if "test -d ~/.asdf/plugins/#{params[:name]}"
+    notifies :run, 'execute[import-release-team-keyring]'
   end
 
-  if params[:name] == 'nodejs'
-    execute 'bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring' do
-      user node[:user]
-      not_if 'test -d ~/.asdf/plugins/nodejs'
-    end
+  execute 'import-release-team-keyring' do
+    command 'bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring'
+    user node[:user]
+    action :nothing
+    only_if { params[:name] == 'nodejs' }
   end
 end
 
