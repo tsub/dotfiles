ee04237976f3b7e0a4ddff85879b2628df2469c6
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2021-02-23 17:37:33 +0900

    Install SafariMarkdownLinker app

--- cookbooks/mas/recipes/mas.rb
+++ cookbooks/mas/recipes/mas.rb
@@ -1,4 +1,5 @@
 mas 'Magnet' { app_id '441258766' }
 mas 'Popclip' { app_id '445189367' }
 mas 'Reeder' { app_id '880001334' }
+mas 'SafariMarkdownLinker' { app_id '1289119450' }
 mas 'bookmarker for pinboard' { app_id '1451400394' }
