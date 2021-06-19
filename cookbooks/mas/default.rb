d8ff2ba9ccb55299d2d38b5e68d150f22ba20a11
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2021-06-19 11:52:03 +0900

    Fix for first installation

--- cookbooks/mas/default.rb
+++ cookbooks/mas/default.rb
@@ -1,5 +1,5 @@
 define :mas, app_id: nil do
-  execute "mas install #{params[:app_id]}" do
+  execute "mas purchase #{params[:app_id]}" do
     user node[:user]
     not_if "mas list | grep #{params[:app_id]}"
   end
