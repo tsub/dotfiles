ddec6430f9758087f328d66890c82d1d312de476
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2021-06-19 11:21:02 +0900

    Skip cookbooks that don't support apple silicon

--- roles/darwin.rb
+++ roles/darwin.rb
@@ -19,6 +19,8 @@ node.reverse_merge!(
   gopath: ENV['GOPATH'] || "#{ENV['HOME']}/go"
 )
 
+arch = run_command('uname -m').stdout.strip # workaround: node['kernel'] return nil when Darwin
+
 include_recipe '../cookbooks/homebrew/default.rb'
 include_recipe '../cookbooks/zsh/default.rb'
 include_recipe '../cookbooks/fish/default.rb'
@@ -27,14 +29,14 @@ include_recipe '../cookbooks/git/default.rb'
 include_recipe '../cookbooks/neovim/default.rb'
 include_recipe '../cookbooks/the_silver_searcher/default.rb'
 include_recipe '../cookbooks/tmux/default.rb'
-include_recipe '../cookbooks/ghg/default.rb'
+include_recipe '../cookbooks/ghg/default.rb' if arch == 'amd64'
 include_recipe '../cookbooks/alacritty/default.rb'
 include_recipe '../cookbooks/goget/default.rb'
 include_recipe '../cookbooks/bingo/default.rb'
 include_recipe '../cookbooks/kitty/default.rb'
 include_recipe '../cookbooks/hyper/default.rb'
-include_recipe '../cookbooks/whalebrew/default.rb'
+include_recipe '../cookbooks/whalebrew/default.rb' if arch == 'amd64'
 include_recipe '../cookbooks/pip/default.rb'
 include_recipe '../cookbooks/krew/default.rb'
 include_recipe '../cookbooks/mas/default.rb'
-include_recipe '../cookbooks/helm_plugins/default.rb'
+include_recipe '../cookbooks/helm_plugins/default.rb' if arch == 'amd64'
