a4dd5ad00a2b15d65a63d488f41bd86fe62623c7
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2021-06-19 11:06:11 +0900

    Use pip3 instead of pip

--- cookbooks/pip/default.rb
+++ cookbooks/pip/default.rb
@@ -1,7 +1,7 @@
 define :pip do
-  execute "pip install --user #{params[:name]}" do
+  execute "pip3 install --user #{params[:name]}" do
     user node[:user]
-    not_if "pip list | grep #{params[:name]}"
+    not_if "pip3 list | grep #{params[:name]}"
   end
 end
 
