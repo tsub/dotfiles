82ac4db077995094ea94be4dc0d50547b8d7d224
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2022-05-21 11:15:35 +0900

    Use go install after golang v1.16

--- cookbooks/goget/default.rb
+++
@@ -1,8 +0,0 @@
-define :goget, binary_name: nil do
-  execute "GOPATH=#{node[:gopath]} go get #{params[:name]}" do
-    user node[:user]
-    not_if "test -x #{node[:gopath]}/bin/#{params[:name].split('/').last.split('@').first}"
-  end
-end
-
-include_recipe 'recipes/goget.rb'
---
+++ cookbooks/goinstall/default.rb
@@ -0,0 +1,10 @@
+package 'golang'
+
+define :goinstall, binary_name: nil, version: 'latest' do
+  execute "GOPATH=#{node[:gopath]} go install #{params[:name]}@#{params[:version]}" do
+    user node[:user]
+    not_if "test -x #{node[:gopath]}/bin/#{params[:name].split('/').last}"
+  end
+end
+
+include_recipe 'recipes/goinstall.rb'
