18fc32620b25142fd4a05adc7a4f002358b1cf8e
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-02-18 17:03:33 +0900

    Improve install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
-bin/setup
+basedir=$(dirname ${0})
+${basedir}/bin/setup
 
 # Homebrew does not allow sudo.
 case "$(uname)" in
-  "Darwin")  bin/mitamae local "$@" lib/recipe.rb ;;
-  *) sudo -E bin/mitamae local "$@" lib/recipe.rb ;;
+  "Darwin")  ${basedir}/bin/mitamae local "$@" ${basedir}/lib/recipe.rb ;;
+  *) sudo -E ${basedir}/bin/mitamae local "$@" ${basedir}/lib/recipe.rb ;;
 esac
