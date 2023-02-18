6851c1edc3b17d13cdd624e61b861e34859961eb
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-02-18 16:25:13 +0900

    Move ~/.gitconfig to ~/.config/git/config

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -2,25 +2,6 @@ arch = run_command('uname -m').stdout.strip # workaround: node['kernel'] return 
 
 ## Git
 
-link File.expand_path('~/.gitconfig') do
-  to File.expand_path("../files/.gitconfig.#{node[:platform]}", __FILE__)
-  user node[:user]
-  force true
-end
-
-template File.expand_path('~/.gitconfig.local') do
-  source File.expand_path('../templates/.gitconfig.local.erb', __FILE__)
-  owner node[:user]
-  group node[:group]
-  not_if 'test -f ~/.gitconfig.local'
-end
-
-link File.expand_path('~/.git_commit_messages') do
-  to File.expand_path('../files/.git_commit_messages', __FILE__)
-  user node[:user]
-  force true
-end
-
 directory File.expand_path('~/.config') do
   user node[:user]
 end
@@ -29,6 +10,13 @@ link File.expand_path('~/.config/git') do
   to File.expand_path('../files/.config/git', __FILE__)
   user node[:user]
   force true
+end
+
+template File.expand_path('~/.config/git/config-local') do
+  source File.expand_path('../templates/.gitconfig-local.erb', __FILE__)
+  owner node[:user]
+  group node[:group]
+  not_if 'test -f ~/.config/git/config-local'
 end
 
 ## tig
