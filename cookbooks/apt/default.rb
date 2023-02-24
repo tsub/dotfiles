b15a8e07a98eda53adc83069b7c2be144406c22d
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-02-24 07:20:00 +0000

    Add apt cookbook for debian

---
+++ cookbooks/apt/default.rb
@@ -0,0 +1,2 @@
+package 'dnsutils'
+package 'ripgrep'
--- roles/debian.rb
+++ roles/debian.rb
@@ -3,3 +3,4 @@ return unless ENV['REMOTE_CONTAINERS']
 
 include_recipe '../cookbooks/tig'
 include_recipe '../cookbooks/diff-highlight'
+include_recipe '../cookbooks/apt'
