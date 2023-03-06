f98ea09a59d5dbe4bbd6917b7f91e448c269db92
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-03-06 09:25:04 +0900

    Install lua-launguage-server with official formula

--- cookbooks/homebrew/recipes/tap.rb
+++ cookbooks/homebrew/recipes/tap.rb
@@ -5,6 +5,5 @@ tap 'homebrew/cask-fonts'
 tap 'homebrew/cask-versions'
 tap 'masutaka/github-nippou'
 tap 'microsoft/git'
-tap 'saadparwaiz1/personal' # for lua-language-server
 tap 'tsub/s3-edit'
 tap 'tsub/yamlls' { uri 'https://github.com/tsub/homebrew-yamlls.git' }
