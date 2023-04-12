a26e307b103049c3fe9c185c67342c110559411a
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-04-12 14:19:51 +0900

    Install kubectl-neat-diff

---
+++ cookbooks/kubectl-neat-diff/default.rb
@@ -0,0 +1,12 @@
+tmp_dir = '/tmp/kubectl-neat-diff'
+
+git tmp_dir do
+  repository 'https://github.com/sh0rez/kubectl-neat-diff'
+  user node[:user]
+  not_if 'which kubectl-neat-diff'
+end
+
+execute 'make install' do
+  cwd tmp_dir
+  not_if 'which kubectl-neat-diff'
+end
--- roles/debian.rb
+++ roles/debian.rb
@@ -4,3 +4,4 @@ return unless ENV['REMOTE_CONTAINERS']
 include_recipe '../cookbooks/tig'
 include_recipe '../cookbooks/diff-highlight'
 include_recipe '../cookbooks/apt'
+include_recipe '../cookbooks/kubectl-neat-diff'
