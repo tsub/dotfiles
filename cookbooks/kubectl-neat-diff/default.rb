2abce3129440daff49f3cfea47ad2b092be5e3df
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-04-12 14:48:26 +0900

    Improve check if kubectl-neat-diff installed

--- cookbooks/kubectl-neat-diff/default.rb
+++ cookbooks/kubectl-neat-diff/default.rb
@@ -1,13 +1,14 @@
 tmp_dir = '/tmp/kubectl-neat-diff'
+install_path = "#{node['gopath']}/bin/kubectl-neat-diff"
 
 git tmp_dir do
   repository 'https://github.com/sh0rez/kubectl-neat-diff'
   user node[:user]
-  not_if 'which kubectl-neat-diff'
+  not_if "test -x #{install_path}"
 end
 
 execute 'make install' do
   cwd tmp_dir
   user node[:user]
-  not_if 'which kubectl-neat-diff'
+  not_if "test -x #{install_path}"
 end
--- roles/debian.rb
+++ roles/debian.rb
@@ -1,6 +1,10 @@
 # Only devcontainer
 return unless ENV['REMOTE_CONTAINERS']
 
+node.reverse_merge!(
+  gopath: ENV['GOPATH'] || "#{ENV['HOME']}/go"
+)
+
 include_recipe '../cookbooks/tig'
 include_recipe '../cookbooks/diff-highlight'
 include_recipe '../cookbooks/apt'
