b1a599d843c24e90737f8fbfb78130b810948c84
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-04-18 16:44:52 +0900

    Update deprecated settings in projekt0n/github-nvim-theme

--- cookbooks/neovim/files/nvim/lua/plugins.lua
+++ cookbooks/neovim/files/nvim/lua/plugins.lua
@@ -200,9 +200,12 @@ return packer.startup(function(use)
       'projekt0n/github-nvim-theme',
       config = function()
         require('github-theme').setup {
-          theme_style = 'dark_default',
-          transparent = true,
+          options = {
+            transparent = true,
+          },
         }
+
+        vim.cmd('colorscheme github_dark')
       end,
     }
   }
