282ac5730abf7b49ecd8ddd2e8cef1325cd06870
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-08-16 22:42:29 +0900

    Remove root_dir settings that do not need to be overwritten default settings

--- cookbooks/neovim/files/nvim/lua/lsp.lua
+++ cookbooks/neovim/files/nvim/lua/lsp.lua
@@ -97,7 +97,6 @@ lspconfig.lua_ls.setup {
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  root_dir = lspconfig.util.find_package_json_ancestor,
   -- See: https://github.com/neovim/nvim-lspconfig/issues/2507
   single_file_support = false,
   init_options = {
@@ -112,7 +111,6 @@ lspconfig.tsserver.setup {
 lspconfig.denols.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  root_dir = lspconfig.util.root_pattern("deno.json"),
 }
 
 -- See: https://github.com/mrjosh/helm-ls
