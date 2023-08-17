0f6b0d52ebff197a7d771c1aabb8cbe4ad92bcfc
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-08-17 22:42:12 +0900

    Install neovim for debian

--- cookbooks/neovim/default.rb
+++ cookbooks/neovim/default.rb
@@ -1,3 +1,19 @@
+if node[:platform] == 'debian'
+  package 'cmake'
+  git File.expand_path("#{Dir.tmpdir}/neovim") do
+    repository 'https://github.com/neovim/neovim.git'
+    user node[:user]
+    depth 1
+    not_if 'which neovim'
+  end
+
+  execute 'make CMAKE_BUILD_TYPE=Release && sudo make install' do
+    cwd File.expand_path("#{Dir.tmpdir}/neovim")
+    user node[:user]
+    not_if 'which neovim'
+  end
+end
+
 directory node[:neovim][:home] do
   user node[:user]
 end
--- roles/debian.rb
+++ roles/debian.rb
@@ -1,12 +1,18 @@
 # Only devcontainer
 return unless ENV['REMOTE_CONTAINERS']
 
+config_home = ENV['XDG_CONFIG_HOME'] || "#{ENV['HOME']}/.config"
+
 node.reverse_merge!(
   gopath: ENV['GOPATH'] || "#{ENV['HOME']}/go",
   goroot: ENV['GOROOT'] || '/usr/local/go',
+  neovim: {
+    home: config_home
+  },
 )
 
 include_recipe '../cookbooks/tig'
 include_recipe '../cookbooks/diff-highlight'
 include_recipe '../cookbooks/apt'
 include_recipe '../cookbooks/kubectl-neat-diff'
+include_recipe '../cookbooks/neovim'
