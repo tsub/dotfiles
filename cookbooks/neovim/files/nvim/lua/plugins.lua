20c5b8da365cdbbd7f121e2f4b8f464916a38c49
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-08-17 23:02:28 +0900

    Fix :TSUpdate error when first installation

--- cookbooks/neovim/files/nvim/lua/plugins.lua
+++ cookbooks/neovim/files/nvim/lua/plugins.lua
@@ -187,7 +187,11 @@ return packer.startup(function(use)
     },
     {
       'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
+      -- See: https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
+      run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+      end,
       config = function()
         require('nvim-treesitter.configs').setup {
           highlight = {
