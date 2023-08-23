b0f49f62fb18c345e2edd42cc6905e347e28f82b
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2023-08-23 17:35:39 +0900

    Remove unnecessary directory

--- cookbooks/opener-client/default.rb
+++ cookbooks/opener-client/default.rb
@@ -2,10 +2,6 @@
 
 package 'netcat'
 
-directory File.expand_path('~/.config') do
-  user node[:user]
-end
-
 link File.expand_path('/usr/local/bin/open') do
   to File.expand_path('../files/open', __FILE__)
   force true
