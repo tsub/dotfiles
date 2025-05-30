d73fa7f7ad7da68d7045813d0a5c52022402520f
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2025-05-30 13:24:20 +0900

    Fix execution user avoid permission error

--- cookbooks/rpbcopyd-client/default.rb
+++ cookbooks/rpbcopyd-client/default.rb
@@ -3,7 +3,6 @@ rpbpaste_path = File.expand_path('/usr/local/biin/pbpaste')
 
 execute "curl -fsSL -o #{rpbcopy_path} https://github.com/tk0miya/rpbcopyd/releases/download/v1.0.0/rpbcopy-v1.0.0-aarch64-unknown-linux-musl" do
   not_if "test -f #{rpbcopy_path}"
-  user node[:user]
 end
 
 file rpbcopy_path do
@@ -12,7 +11,6 @@ end
 
 execute "curl -fsSL -o #{rpbpaste_path} https://github.com/tk0miya/rpbcopyd/releases/download/v1.0.0/rpbpaste-v1.0.0-aarch64-unknown-linux-musl" do
   not_if "test -f #{rpbpaste_path}"
-  user node[:user]
 end
 
 file rpbpaste_path do
