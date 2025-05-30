a4c258c1a5ccad938b4f2ec89d56330e01d16d47
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2025-05-30 12:10:45 +0900

    Add rpbcopyd-client cookbook and rename rpbcopyd to rpbcopyd-server

---
+++ cookbooks/rpbcopyd-client/default.rb
@@ -0,0 +1,20 @@
+rpbcopy_path = File.expand_path('/usr/local/bin/pbcopy')
+rpbpaste_path = File.expand_path('/usr/local/biin/pbpaste')
+
+execute "curl -fsSL -o #{rpbcopy_path} https://github.com/tk0miya/rpbcopyd/releases/download/v1.0.0/rpbcopy-v1.0.0-aarch64-unknown-linux-musl" do
+  not_if "test -f #{rpbcopy_path}"
+  user node[:user]
+end
+
+file rpbcopy_path do
+  mode '0755'
+end
+
+execute "curl -fsSL -o #{rpbpaste_path} https://github.com/tk0miya/rpbcopyd/releases/download/v1.0.0/rpbpaste-v1.0.0-aarch64-unknown-linux-musl" do
+  not_if "test -f #{rpbpaste_path}"
+  user node[:user]
+end
+
+file rpbpaste_path do
+  mode '0755'
+end
