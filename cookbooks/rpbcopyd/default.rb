fb4fb2ac084d51ee12f00d90f3b31edb5af78252
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2025-05-30 11:15:30 +0900

    Add rpbcopyd cookbook

---
+++ cookbooks/rpbcopyd/default.rb
@@ -0,0 +1,19 @@
+rpbcopyd_path = File.expand_path('~/.bin/rpbcopyd')
+
+directory File.expand_path('~/.bin') do
+  user node[:user]
+end
+
+execute "curl -fsSL -o #{rpbcopyd_path} https://github.com/tk0miya/rpbcopyd/releases/download/v1.0.0/rpbcopyd-v1.0.0-aarch64-apple-darwin" do
+  not_if "test -f #{rpbcopyd_path}"
+  user node[:user]
+end
+
+file rpbcopyd_path do
+  mode '0755'
+end
+
+execute "#{rpbcopyd_path} -d" do
+  not_if 'pgrep -fq rpbcopyd'
+  user node[:user]
+end
