d9ba799a50631201cfa52ce6da10466a51f1bf0e
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2025-06-02 08:32:45 +0900

    Suppress curl output

--- cookbooks/helm_ls/default.rb
+++ cookbooks/helm_ls/default.rb
@@ -4,7 +4,7 @@ arch = run_command('uname -m').stdout.strip.sub('aarch64', 'arm') # workaround: 
 url = "https://github.com/mrjosh/helm-ls/releases/download/master/helm_ls_#{os}_#{arch}"
 puts url
 
-execute "curl -fSL -o /usr/local/bin/helm-ls #{url}" do
+execute "curl -fsSL -o /usr/local/bin/helm-ls #{url}" do
   not_if 'test -x /usr/local/bin/helm-ls'
 end
 
