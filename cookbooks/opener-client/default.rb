a5c8f3113d4ceb0c5e4ecb7e3cea6d3784e984e4
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2025-07-01 12:02:17 +0900

    Support debian 12

--- cookbooks/opener-client/default.rb
+++ cookbooks/opener-client/default.rb
@@ -1,6 +1,10 @@
 # See: https://github.com/superbrothers/opener#remote-environment
 
-package 'netcat'
+if node['platform'] == 'debian'
+  package 'netcat-openbsd'
+else
+  package 'netcat'
+end
 
 link File.expand_path('/usr/local/bin/open') do
   to File.expand_path('../files/open', __FILE__)
