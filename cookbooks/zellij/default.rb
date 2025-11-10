a5ea665b126f073ce63ad5a6df9b1244a6e29e52
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2025-11-10 16:09:28 +0900

    Create zellij config directory

--- cookbooks/zellij/default.rb
+++ cookbooks/zellij/default.rb
@@ -1,3 +1,7 @@
+directory File.expand_path('~/.config/zellij') do
+  user node[:user]
+end
+
 link File.expand_path('~/.config/zellij/config.kdl') do
   to File.expand_path('../files/config.kdl', __FILE__)
   user node[:user]
