7cfa7701de675617ecf687acab923639bceb2d40
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2025-12-08 09:54:57 +0900

    Enable show_hidden for oil.nvim

--- cookbooks/neovim/files/nvim/lua/plugins/init.lua
+++ cookbooks/neovim/files/nvim/lua/plugins/init.lua
@@ -197,7 +197,11 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      view_options = {
+        show_hidden = true,
+      }
+    },
     -- Optional dependencies
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
