dc37e2d8a8aab06a9a72d215638662d835c084d5
Tsubasa Takayama <10208211+tsub@users.noreply.github.com>
2026-02-03 11:42:35 +0900

    Move ~/.colima to ~/.config/colima

--- cookbooks/colima/default.rb
+++ cookbooks/colima/default.rb
@@ -1,4 +1,8 @@
-link File.expand_path('~/.colima/default/colima.yaml') do
+directory File.expand_path('~/.config/colima/default') do
+  user node[:user]
+end
+
+link File.expand_path('~/.config/colima/default/colima.yaml') do
   to File.expand_path('../files/colima.yaml', __FILE__)
   user node[:user]
   force true
